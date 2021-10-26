57b70b944b753da58b7bc7c810f174836abaecf8
Iisakki Säkkinen <einoiisko@gmail.com>
2021-10-26 18:36:38 +0300

    Added global definitions

---
+++ game-of-life/Global.h
@@ -0,0 +1,14 @@
+#pragma once
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 800
+#define WINDOW_MARGIN 80
+
+#define CELL_MARGIN 0.5f
+
+#define MAX_FRAMERATE 60.f
+#define MIN_FRAMERATE 1.f
+
+#define MAX_WORLD_SIZE 200
+#define MIN_WORLD_SIZE 3
+
+#define BACKGROUND_COLOR { 50, 50, 50 }
